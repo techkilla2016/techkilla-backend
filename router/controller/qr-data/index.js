42a7e6bd21031f7ee348da39815665791fd462ad
Mukesh Singh <118162167+kabawat@users.noreply.github.com>
2023-10-09 16:58:43 +0530

    save data after responce

--- router/controller/qr-data/index.js
+++ router/controller/qr-data/index.js
@@ -1,33 +1,43 @@
-const { qrModel } = require("../../../module")
+const { qrModel } = require("../../../module");
 
 async function qrSolution(req, res) {
     try {
-        const data = req.body
-        const model = new qrModel({
-            name: data?.name,
-            email: data?.email,
-            country: data?.country,
-            phone: data?.phone,
-            job: data?.job,
-            company: data?.company,
-            utm_source: data?.utm_source,
-            utm_medium: data?.utm_medium,
-            utm_campaign: data?.utm_campaign
-        })
+        const data = req.body;
 
-        const isSave = await model.save()
-        if (!isSave) {
-            throw new Error('something wrong!')
-        }
+        // Send the response first
         res.status(200).json({
             status: true,
-            message: ""
-        })
+            message: "Response sent",
+        });
+
+        // Introduce a delay of, for example, 1 second (1000 milliseconds)
+        setTimeout(async () => {
+            const model = new qrModel({
+                name: data?.name,
+                email: data?.email,
+                country: data?.country,
+                phone: data?.phone,
+                job: data?.job,
+                company: data?.company,
+                utm_source: data?.utm_source,
+                utm_medium: data?.utm_medium,
+                utm_campaign: data?.utm_campaign,
+            });
+
+            const isSave = await model.save();
+
+            if (!isSave) {
+                console.error('Something went wrong while saving data.');
+            } else {
+                console.log('Data saved successfully.');
+            }
+        }, 1000); // 1000 milliseconds = 1 second
     } catch (error) {
         res.status(405).json({
             status: false,
-            message: error.message
-        })
+            message: error.message,
+        });
     }
 }
-module.exports = qrSolution
+
+module.exports = qrSolution;
